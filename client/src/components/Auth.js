257a65237a2f68bb0f6811a87a64cebcca86ccca
Sean Park <seanpark@Seans-MacBook-Air-3.local>
2025-03-11 15:55:06 +1100

    Edited URL from local to railway

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -26,7 +26,7 @@ const Auth = () => {
     }
 
     const response = await fetch(
-      `${process.env.REACT_APP_SERVERURL}/${endpoint}`,
+      `${process.env.REACT_APP_API_URL}/${endpoint}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
