b39faa817f7040e8f2bc3597f78bb3d9b9cbf678
Sean Park <seanpark@Seans-MacBook-Air-3.local>
2025-03-12 11:00:09 +1100

    Apply this fix to App.js (remove setCookie, removeCookie).

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 
 const App = () => {
-  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const [cookies] = useCookies(null);
   const authToken = cookies.AuthToken;
   const userEmail = cookies.Email;
   const [tasks, setTasks] = useState(null);
