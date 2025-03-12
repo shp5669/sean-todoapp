77d383a9c03ee48a6e02104ecb577f92c06283f0
Sean Park <seanpark@Seans-MacBook-Air-3.local>
2025-03-12 12:12:11 +1100

    Branch checking

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT ?? 5000;
+const PORT = process.env.PORT ?? 8000;
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
